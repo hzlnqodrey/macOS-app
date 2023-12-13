9c70ca9bdc7a3a887e3deedc2311cb756727f13f
Hazlan Qodrey <hazlanmuhammadqodri3@gmail.com>
2023-12-13 14:22:03 +0700

    Initial Commit

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by Hazlan Muhammad Qodri on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
